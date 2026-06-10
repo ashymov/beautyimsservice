beeaea5c2ba24ddcfaf398e9eca912ba52fe2aaa
Ashymov Aziret <aziretashym@gmail.com>
2026-06-10 17:17:44 +0600

    Доработана логика пользователей

--- src/main/java/com/sashymov/beautyimsservice/controllers/UserController.java
+++ src/main/java/com/sashymov/beautyimsservice/controllers/UserController.java
@@ -3,47 +3,64 @@ package com.sashymov.beautyimsservice.controllers;
 import com.sashymov.beautyimsservice.models.dto.UserDto;
 import com.sashymov.beautyimsservice.models.entities.User;
 import com.sashymov.beautyimsservice.respones.Response;
-import com.sashymov.beautyimsservice.services.FileService;
 import com.sashymov.beautyimsservice.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-    @Autowired
-    private UserService userService;
-    @Autowired
-    private FileService fileService;
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/create")
     public Response createUser(@RequestBody UserDto userDto) {
-       return userService.saveUser(userDto);
+        return userService.saveUser(userDto);
     }
+
+    @PutMapping("/update/{id}")
+    public Response updateUser(@PathVariable Long id, @RequestBody UserDto userDto) {
+        return userService.updateUser(id, userDto);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public Response deleteUser(@PathVariable Long id) {
+        return userService.deleteUser(id);
+    }
+
+    @GetMapping("/getAll")
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @GetMapping("/getById/{id}")
+    public User getById(@PathVariable Long id) {
+        return userService.findById(id);
+    }
+
     @GetMapping("/getByUsername")
     public Response getUserByUsername(@RequestParam String username) {
         return userService.getUserByUsername(username);
     }
 
     @GetMapping("/getByEmail")
-    public User getUserByEmail(@RequestParam String email) {
+    public Response getUserByEmail(@RequestParam String email) {
         return userService.getUserByEmail(email);
     }
 
-    @GetMapping("/getUserByPhone")
+    @GetMapping("/getByPhone")
     public Response getUserByPhone(@RequestParam String phone) {
         return userService.getUserByPhone(phone);
     }
 
-    @GetMapping("/getById")
-    public User getById(@RequestParam Long id) {
-        return userService.findById(id);
-    }
-
     @PostMapping(value = "/upload", consumes = "multipart/form-data")
-    public Response upload(@RequestPart MultipartFile file,@RequestParam Long userId) {
-        return userService.upload(file,userId);
+    public Response upload(@RequestPart MultipartFile file, @RequestParam Long userId) {
+        return userService.upload(file, userId);
     }
 }
